e88e698fe06e9ff867186bb8816fb9389d4958b6
manutolosa <manutolosa.garcia@gmail.com>
2022-02-15 23:06:15 -0500

    cambios en función home

--- movie/views.py
+++ movie/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Welcome to home page</h1>')
+    return HttpResponse('<h1>Welcome to home page</h1>',{'name':'Manuela'})
