440caa9536402d11c493ead4ef35fe6e2493d1a3
manutolosa <manutolosa.garcia@gmail.com>
2022-03-03 10:54:46 -0500

    Visualizacion de peliculas

--- movie/views.py
+++ movie/views.py
@@ -1,6 +1,18 @@
+from re import search
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import HttpResponse
 
+import movie
+
+from .models import Movie
 
 def home(request):
-    return render(request, 'home.html', {'name':'Manuela'})
+    searchTerm = request.GET.get('searchMovie')
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm, 'movies':movies})
+
+def about(request):
+    return HttpResponse('Welcome to About page</h1>')
